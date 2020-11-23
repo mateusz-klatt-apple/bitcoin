73e3f3e2bcf3fdaec43aaa32bb5c71820e3d7af5
Mateusz Klatt <mateusz@klatt.ie>
2020-11-23 15:00:05 +0000

    Commercial National Security Algorithm Suite

--- build_msvc/sign.bat
+++ build_msvc/sign.bat
@@ -1,13 +1,13 @@
 @echo off
 set CodeSignDescription=Bitcoin
 set CodeSignDescriptionUrl=https://github.com/mateusz-klatt/bitcoin
-set CodeSignFileDigest=sha256
+set CodeSignFileDigest=sha384
 set CodeSignTimestampUrl=http://timestamp.sectigo.com
-set CodeSignTimestampDigest=sha256
+set CodeSignTimestampDigest=sha384
 set AzureKeyVaultUrl=https://codesignkeyvault.vault.azure.net/
 set AzureKeyVaultClientId=38a04401-d8f1-4d37-ba2f-15b6d53f6cdf
 set AzureKeyVaultClientSecret=%CODE_SIGNING_CLOUD%
-set AzureKeyVaultCertificate=SectigoHome
+set AzureKeyVaultCertificate=MateuszKlatt
 set SignFile=%APPVEYOR_BUILD_FOLDER%\bitcoin-%APPVEYOR_BUILD_VERSION%-setup.exe
 
 dotnet -d tool restore
